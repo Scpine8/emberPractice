32d7ddcb3a1c8bc9abbe50c8a35f8c8a64d9d593
Scpine8 <Scpine8@gmail.com>
2021-07-08 16:19:55 -0400

    test: added tests to super-rentals-tests

--- tests/acceptance/super-rentals-test.js
+++ tests/acceptance/super-rentals-test.js
@@ -9,11 +9,25 @@ module('Acceptance | super rentals', function(hooks) {
     await visit('/'); // wait until the page loads to run tests
 
     assert.equal(currentURL(), '/');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
     assert.dom('h2').hasText('Welcome to Super Rentals!');
 
     assert.dom('.jumbo a.button').hasText('About Us');
     await click('.jumbo a.button');
 
     assert.equal(currentURL(), '/about')
+  });
+
+  test('visiting /about', async function(assert) {
+    await visit('/about'); // wait until the page loads to run tests
+
+    assert.equal(currentURL(), '/about');
+    assert.dom('h2').hasText('About Super Rentals');
+
+    assert.dom('.jumbo a.button').hasText('Contact Us');
+    await click('.jumbo a.button');
+
+    assert.equal(currentURL(), '/getting-in-touch')
   });
 });
